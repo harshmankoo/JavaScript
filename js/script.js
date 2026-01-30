6aa10a8af39aa4b2395859c1ba7d24ac2350a404
harshmankoo <ravkiratsingh9988@gmial.com>
2026-01-30 17:46:19 +0530

    Java script task 1 and 2

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// var fword = "Hello";
+// console.log(fword);
+// var regards = "Good Morning";
+// console.log(regards);
+// var Fname = prompt("Enter Your Name");
+// console.log(`This is my first Js task ${fword} ${regards} ${Fname}`);
+ 
+
+var fname = "Harshdeep Singh"
+console.log(fname);
+var collage = "Guru Nanak Dev Engineering Collage"
+console.log (collage)
+var course = "MCA"
+console.log(course);
