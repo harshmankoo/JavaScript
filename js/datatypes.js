4a81313eda5cc7ede195c013c0fb6a4579c231fb
harshmankoo <ravkiratsingh9988@gmial.com>
2026-02-02 17:46:33 +0530

    concepts of datatypes and operators

---
+++ js/datatypes.js
@@ -0,0 +1,128 @@
+// // // DataTypes
+// // // Primitive DataTypes
+// // Primitive data types are the built-in data types provided by JavaScript. They represent single values and are not mutable. JavaScript supports the following primitive data types:
+
+// // // numbers
+// // var age = 15;
+// // console.log(age)
+// // console.log(typeof age)
+
+// // // string
+
+// // var firstName = "12apple";
+// // console.log(firstName)
+// // console.log(typeof firstName)
+
+// // // Boolean
+
+// // // true and false
+// // // 0 false and 1 true 
+
+// // var adult = true;
+// // console.log(adult)
+// // console.log(typeof adult)
+
+// // // BigIntegars
+
+// // var numbers = 1.22222564616;
+// // console.log(numbers)
+// // console.log(typeof numbers)
+
+// // // undefined
+
+// // var lastName;
+// // console.log(lastName)
+// // console.log(typeof lastName)
+
+// // // Non Primitive DataTypes
+
+// // // arrays
+// // // Objects
+
+// // var fruits = ["apple","banana","pineapple","Guava",true, 12,3151]
+// // console.log(fruits.length)
+// // console.log(fruits[1]) //array accessing
+
+// // var obj = {
+// //     // key:value,
+// //     fName: "Harsh",
+// //     lName: "Singh"
+// // }
+
+// // console.log(obj
+
+
+
+
+
+
+
+
+
+
+// // Primitive Datatypes
+
+// // Primitive data types are the built-in data types provided by JavaScript. They represent single values and are not mutable. JavaScript supports the following primitive data types:
+
+// // 1.numbers
+
+// var age = 222;
+// console.log(age)
+// console.log(typeof age)
+
+// // 2. string
+// //  ------------------string always use in the double colons
+
+//  var fullname = "1234Harshdeep singh";
+//  console.log(fullname);
+//  console.log(typeof fullname);
+
+// //  3. Boolean
+// //  ----------------------- boolean denoted as TRUE AND FALSE--- where true value is 1 and the false value is 0
+
+
+// // -------------true---------------
+// var information = true ;    
+// console.log(information)
+// console.log(typeof information)
+// // -------------true---------------
+// var information = false;
+// console.log(information)
+// console.log(typeof information)
+
+// // 4. big intergers
+
+// var numbers = 99.8989383939393
+// console.log(numbers)
+// console.log(typeof numbers)
+
+// // 5.undefined
+
+// var carname;
+// console.log(carname)
+// console.log(typeof carname)
+
+
+// // ---------------NON PRIMITIVE DATATYPES---------
+
+// // non-primitive data types, also known as reference types or objects, are used to store collections of data or more complex entities
+  
+// // 1. ARRAYS  -------  ALWAYS START WITH THE SQUARE BRACKETS[]
+
+// var carbrands = [ "Ford",  "Chevrolet",    " GMC",    "Cadillac",    "Jeep"]
+// console.log(carbrands)
+// console.log(carbrands[3])   //accessig a specific array
+// console.table(carbrands)  // for making table of the data
+
+// // now if we want a specific array from all the array so we can call a specific arrra by calling its value
+// // we can call its value in squaree brackets[]
+  
+
+// // 2. OBJECTS--------------ALWAYS START WITH THE CURLY BRACKETS {}
+
+// // key:value,
+// const obj = {
+//     firstName:"Harshdeep",
+//     lastName:"Singh",
+// }
+//  console.log(obj);
--- js/script.js
+++ js/script.js
@@ -6,9 +6,18 @@
 // console.log(`This is my first Js task ${fword} ${regards} ${Fname}`);
  
 
-var fname = "Harshdeep Singh"
-console.log(fname);
-var collage = "Guru Nanak Dev Engineering Collage"
-console.log (collage)
-var course = "MCA"
-console.log(course);
+// var fname = "Harshdeep Singh"
+// console.log(fname);
+// var collage = "Guru Nanak Dev Engineering Collage"
+// console.log (collage)
+// var course = "MCA"
+// console.log(course);
+
+// var schoolName = "SHSPSRSECSCHOOL"
+// console.log(schoolName);
+// var area = "New Janta Nagar"
+// console.log (area);
+// var pincode ="141003";
+// console.log(pincode)
+// var password=prompt("Please Enter your Password ")
+// console.log(`Here is the info of the AREA ${schoolName} ${area} ${pincode}`);
